5d8197b0ea6bc8daa4ef174c2409a5d86fcf7bbf
PerpetuumG <perpetuumg@gmail.com>
2022-08-08 18:37:00 +0300

    Add block Answers

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,15 +1,91 @@
 import React, {Component} from "react";
 import classes from "./Quzi.css";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        activeQuestion: 0,
+        answerState: null,
+        quiz: [
+            {
+                id: 1,
+                question: 'Какого цвета небо?',
+                rightAnswerId: 2,
+                answers: [
+                    {text: 'Черный', id: 1},
+                    {text: 'Синий', id: 2},
+                    {text: 'Красный', id: 3},
+                    {text: 'Зеленый', id: 4}
+                ]
+            },
+            {
+                id: 2,
+                question: 'В каком году основали Санкт-Петербург?',
+                rightAnswerId: 3,
+                answers: [
+                    {text: '1700', id: 1},
+                    {text: '1702', id: 2},
+                    {text: '1703', id: 3},
+                    {text: '1803', id: 4}
+                ]
+            }
+        ]
+    }
+
+    onAnswerClickHandler = (answerId) => {
+        if(this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if(this.state.answerState[key] === 'success') {
+                return
+            }
+        }
+
+        const question = this.state.quiz[this.state.activeQuestion]
+        if (question.rightAnswerId === answerId) {
+
+            this.setState({
+                answerState: {[answerId]: 'success'}
+            })
+
+            const timeOut = window.setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    console.log('Finished')
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+
+                window.clearTimeout(timeOut)
+            }, 1000)
+
+        } else {
+            this.setState({
+                answerState: {[answerId]: 'error'}
+            })
+        }
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
     render() {
-        return(
+        return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+                <div className={classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+
+                    <ActiveQuiz
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        question={this.state.quiz[this.state.activeQuestion].question}
+                        onAnswerClick={this.onAnswerClickHandler}
+                        quizLeng={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
+                        state={this.state.answerState}
+                    ></ActiveQuiz>
+                </div>
             </div>
         )
     }
