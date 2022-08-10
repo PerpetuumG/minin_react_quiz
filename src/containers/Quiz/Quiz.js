fae4f2b49571954413fa90cb9c2104beddf0932c
PerpetuumG <perpetuumg@gmail.com>
2022-08-10 18:25:31 +0300

    Add button and styles

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
 import classes from "./Quzi.css";
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
 
 class Quiz extends Component {
     state = {
+        results: {},
+        isFinished: false ,
         activeQuestion: 0,
         answerState: null,
         quiz: [
@@ -33,23 +36,31 @@ class Quiz extends Component {
     }
 
     onAnswerClickHandler = (answerId) => {
-        if(this.state.answerState) {
+        if (this.state.answerState) {
             const key = Object.keys(this.state.answerState)[0]
-            if(this.state.answerState[key] === 'success') {
+            if (this.state.answerState[key] === 'success') {
                 return
             }
         }
 
         const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+
         if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
 
             this.setState({
-                answerState: {[answerId]: 'success'}
+                answerState: {[answerId]: 'success'},
+                results: results
             })
 
             const timeOut = window.setTimeout(() => {
                 if (this.isQuizFinished()) {
-                    console.log('Finished')
+                    this.setState({
+                        isFinished: true
+                    })
                 } else {
                     this.setState({
                         activeQuestion: this.state.activeQuestion + 1,
@@ -61,8 +72,10 @@ class Quiz extends Component {
             }, 1000)
 
         } else {
+            results[question.id] = 'error'
             this.setState({
-                answerState: {[answerId]: 'error'}
+                answerState: {[answerId]: 'error'},
+                results: results
             })
         }
     }
@@ -71,20 +84,37 @@ class Quiz extends Component {
         return this.state.activeQuestion + 1 === this.state.quiz.length
     }
 
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
+        })
+    }
+
     render() {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
 
-                    <ActiveQuiz
-                        answers={this.state.quiz[this.state.activeQuestion].answers}
-                        question={this.state.quiz[this.state.activeQuestion].question}
-                        onAnswerClick={this.onAnswerClickHandler}
-                        quizLeng={this.state.quiz.length}
-                        answerNumber={this.state.activeQuestion + 1}
-                        state={this.state.answerState}
-                    ></ActiveQuiz>
+                    {
+                        this.state.isFinished
+                            ? <FinishedQuiz
+                                results={this.state.results}
+                                quiz={this.state.quiz}
+                                onRetry={this.retryHandler}
+                            ></FinishedQuiz>
+                            : <ActiveQuiz
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                question={this.state.quiz[this.state.activeQuestion].question}
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLeng={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1}
+                                state={this.state.answerState}
+                            ></ActiveQuiz>
+                    }
                 </div>
             </div>
         )
